de51a7f20ebc8091379e57ae529fe5d9faeadfb6
Hitesh <h08rajbhar@gmail.com>
2023-02-16 20:51:50 +0530

    update product

--- controllers/productController.js
+++ controllers/productController.js
@@ -57,8 +57,42 @@ const createProduct = asyncHandler(async (req, res) => {
   });
 });
 
+/**-----------------------------------------------
+ * @desc     update product  (only admin)
+ * @route   /api/products/:id
+ * @method  PUT
+ * @access  private
+ ------------------------------------------------*/
+const updateProduct = asyncHandler(async (req, res) => {
+  const { name, price, description, image, brand, category, countInStock } =
+    req.body;
+
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    product.name = name;
+    product.price = price;
+    product.description = description;
+    product.image = image;
+    product.brand = brand;
+    product.category = category;
+    product.countInStock = countInStock;
+
+    const updatedProduct = await product.save();
+    res.status(200).json({
+      message: 'Product Updated Successfully',
+      product: updatedProduct,
+    });
+  } else {
+    res.status(404).json({
+      message: 'Product not found',
+    });
+  }
+});
+
 module.exports = {
   getProducts,
   getProductById,
   createProduct,
+  updateProduct,
 };
--- routes/ProductRoute.js
+++ routes/ProductRoute.js
@@ -3,16 +3,14 @@ const {
   getProducts,
   getProductById,
   createProduct,
+  updateProduct,
 } = require('../controllers/productController');
 const { protect, admin } = require('../middlewares/authMiddleware');
 
 // /api/products
-router.route('/').get(getProducts);
-
-// /api/products
-router.route('/').post(protect, admin, createProduct);
+router.route('/').get(getProducts).post(protect, admin, createProduct);
 
 // /api/products/:id
-router.route('/:id').get(getProductById);
+router.route('/:id').get(getProductById).put(protect, admin, updateProduct);
 
 module.exports = router;
