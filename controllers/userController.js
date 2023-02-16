7056241dfa02747676bfc3ff0cadf5b4a1e53b09
Hitesh <h08rajbhar@gmail.com>
2023-02-16 17:27:10 +0530

    update profile

--- controllers/userController.js
+++ controllers/userController.js
@@ -70,7 +70,64 @@ const registerUser = asyncHandler(async (req, res) => {
   });
 });
 
+/**-----------------------------------------------
+ * @desc    get user profile
+ * @route   /api/user/:id
+ * @method  GET
+ * @access  private
+ ------------------------------------------------*/
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404).json({
+      message: 'User not Found',
+    });
+  }
+});
+
+/**-----------------------------------------------
+ * @desc    update user profile
+ * @route   /api/user/profile
+ * @method  PUT
+ * @access  private
+ ------------------------------------------------*/
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    if (req.body.password) {
+      user.password = bcrypt.hashSync(req.body.password, 8);
+    }
+
+    const updatedUser = await user.save();
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+      token: generateToken(updatedUser._id),
+    });
+  } else {
+    res.status(404).json({
+      message: 'user not found',
+    });
+  }
+});
+
 module.exports = {
   loginUser,
   registerUser,
+  getUserProfile,
+  updateUserProfile,
 };
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,10 +1,22 @@
 const router = require('express').Router();
-const { loginUser, registerUser } = require('../controllers/userController');
+const {
+  loginUser,
+  registerUser,
+  getUserProfile,
+  updateUserProfile,
+} = require('../controllers/userController');
+const { protect } = require('../middlewares/authMiddleware');
 
 // /api/users/login
 router.route('/login').post(loginUser);
 
 // /api/users/register
 router.route('/register').post(registerUser);
+
+// /api/users/:id
+router.route('/:id').get(protect, getUserProfile);
+
+// /api/users/profile
+router.route('/profile').put(protect, updateUserProfile);
 
 module.exports = router;
